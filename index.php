55133aca248b6954a07adad22399896ad8226985
Per Kristian <per-kristian.svanberg@klaravik.se>
2022-12-02 17:29:02 +0100

    WIP

--- index.php
+++ index.php
@@ -1,5 +1,39 @@
-<?php
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="public/dist/style.css">
+    <title>Invoices</title>
+</head>
+<body>
+    <?php
+    require_once __DIR__ . "/vendor/autoload.php";
 
-require_once __DIR__ . "/vendor/autoload.php";
+    use Dotenv\Dotenv;
+    use Invoices\Repositories\Database;
 
-// Silence is golden
+
+    (Dotenv::createImmutable(__DIR__))->load();
+    $db = new Database();
+
+    $sql = "SELECT * FROM users";
+    $stmt = $db->connection()->query($sql);
+    $users = $stmt->fetchAll(PDO::FETCH_ASSOC);
+
+    if ($users) {
+        echo '<pre>';
+        foreach ($users as $user) {
+            var_dump($user);
+        }
+        echo '</pre>';
+    }
+    ?>
+    <script src="public/dist/app.js"></script>
+</body>
+</html>
+
+
+
