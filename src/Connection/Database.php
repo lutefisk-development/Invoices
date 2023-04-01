5a475d4f3c6ad1ac715c79eff9cc79ec06eec9b9
Per Kristian <per-kristian.svanberg@klaravik.se>
2023-04-01 13:45:43 +0200

    Created class responsible for connecting to the database

--- src/Connection/Database.php
+++ src/Connection/Database.php
@@ -1,8 +1,30 @@
 <?php
+declare(strict_types=1);
 
 namespace App\Connection;
 
+use PDO;
+use PDOException;
+
 class Database
 {
+    /**
+     * @return PDO|null
+     */
+    public static function &connect(): ?PDO
+    {
+        try {
+            $conn = new PDO(
+                "mysql:host={$_ENV['DB_HOST']};dbname={$_ENV['DB_NAME']}",
+                $_ENV['DB_USERNAME'],
+                $_ENV['DB_PASSWORD']
+            );
+            $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        } catch (PDOException $e) {
+            echo 'PDO Error: ' . $e->getMessage();
+            $conn = null;
+        }
 
+        return $conn;
+    }
 }
